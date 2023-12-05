01355b326d8f795cb70eef898b06ca81b300294e
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2023-12-05 18:07:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function clickPictureChangeColor(){
+    let picture = document.getElementById("blue"); 
+    picture.style.backgroundColor = "green";
+    picture.style.width = "50px";
+    picture.style.height = "200px";
+  }
+  
+  function clickPictureAddBorder(){
+    var picture = document.getElementById("black");
+    console.log("this is the fetched element:", picture); 
+    picture.style.backgroundColor = "#ffffff"; 
+    picture.style.border = "2px dotted black"; 
+    document.body.style.backgroundColor = "beige";
+  }
+
+  function clickPictur2ChangeColor(){
+    let picture2 = document.getElementById("blue"); 
+    picture2.style.backgroundColor = "green";
+    picture2.style.width = "50px";
+    picture2.style.height = "200px";
+  }
+  
+  function clickPicture2AddBorder(){
+    var picture2 = document.getElementById("brown");
+    console.log("this is the fetched element:", picture2); 
+    picture2.style.backgroundColor = "#ffffff"; 
+    picture2.style.border = "2px dotted black"; 
+    document.body.style.backgroundColor2 = "beige";
+  }
+
+  function clickPictur3ChangeColor(){
+    let picture3 = document.getElementById("blue"); 
+    picture3.style.backgroundColor = "green";
+    picture3.style.width = "50px";
+    picture3.style.height = "200px";
+  }
+  
+  function clickPicture3AddBorder(){
+    var picture3 = document.getElementById("white");
+    console.log("this is the fetched element:", picture3); 
+    picture3.style.backgroundColor = "#ffffff"; 
+    picture3.style.border = "2px dotted black"; 
+    document.body.style.backgroundColor3 = "beige";
+  }
